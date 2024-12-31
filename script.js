637a5a07d90d1361823973f1b8535dcdae5b4847
Deepak8711 <121600011+Deepak8711@users.noreply.github.com>
2024-12-31 09:26:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-function stringChop(str, size) {
+/*function stringChop(str, size) {
   // your code here
 }
 
 // Do not change the code below
 const str = prompt("Enter String.");
 const size = prompt("Enter Chunk Size.");
+alert(stringChop(str, size));*/
+
+function stringChop(str, size) {
+  // your code here
+	if (!str) return [];
+  
+  const result = [];
+  
+
+  for (let i = 0; i < str.length; i += size) {
+    result.push(str.slice(i, i + size));
+  }
+  
+  return result;
+}
+
+// Do not change the code below
+const str = prompt("Enter String.");
+const size = prompt("Enter Chunk Size.");
 alert(stringChop(str, size));
+ 
